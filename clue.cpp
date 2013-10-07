dee14d3d90cc2b8b6c5befee773ecb9a9f30cbba
Liam Walsh <liammail06@gmail.com>
2013-10-07 15:20:47 +0100

    added clue class files

---
+++ clue.cpp
@@ -0,0 +1,11 @@
+#include "clue.h"
+
+Clue::Clue(string description)
+{
+    this->description = description;
+}
+
+Clue::getDescription()
+{
+    return description;
+}
---
+++ clue.h
@@ -0,0 +1,14 @@
+#ifndef CLUE_H
+#define CLUE_H
+
+class Clue
+{
+    private:
+        string description;
+
+    public:
+        Clue();
+        string getDescription();
+};
+
+#endif // CLUE_H
