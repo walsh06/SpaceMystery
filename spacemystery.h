2be1630a94b3065512921b65ab8fe5e29cefdf1b
Liam Walsh <liammail06@gmail.com>
2013-10-09 10:29:55 +0100

    Added files removed lots of bugs

---
+++ spacemystery.h
@@ -0,0 +1,17 @@
+#ifndef SPACEMYSTERY_H
+#define SPACEMYSTERY_H
+
+#include "Player.h"
+#include "Room.h"
+#include "Clue.h"
+#include "Person.h"
+
+ using namespace std;
+
+class SpaceMystery
+{
+public:
+    SpaceMystery();
+};
+
+#endif // SPACEMYSTERY_H
