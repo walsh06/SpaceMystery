d719e54deaadbb90a9cbdc86a8b75a93dceb8651
HallCat <cathal99h@gmail.com>
2013-10-23 23:47:28 +0100

    Updated mainwindow

--- mainwindow.h
+++ mainwindow.h
@@ -21,6 +21,14 @@ public slots:
 private slots:
     void on_pushButton_2_clicked();
 
+    void on_pushButton_19_clicked();
+
+    void on_chefButton_clicked();
+
+    void on_comboBox_currentIndexChanged(int index);
+
+    void on_pushButton_5_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
